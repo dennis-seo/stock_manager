b855b683bab035c81357952eaf4b843702e42fe5
kyle.jang <kyle.jang@kakaocorp.com>
2020-05-12 21:55:43 +0900

    [*] Excel 파일 생성(미완성)

---
+++ app/src/main/java/com/deky/productmanager/excel/ExcelConverterTask.kt
@@ -0,0 +1,237 @@
+package com.deky.productmanager.excel
+
+import android.os.AsyncTask
+import com.deky.productmanager.database.entity.Product
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_AMOUNT
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_CONDITION
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_IMAGE
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_LABEL
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_LOCATION
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_MANUFACTURER
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_MANUFACTURE_DATE
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_MODEL
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_NAME
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_NO
+import com.deky.productmanager.excel.ColumnInfo.Companion.CELL_INDEX_SIZE
+import com.deky.productmanager.util.DKLog
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.CellType
+import java.io.File
+
+/**
+ * Copyright (C) 2020 Kakao corp. All rights reserved.
+ *
+ */
+class ExcelConverterTask private constructor(
+    private val productList: List<Product>,
+    private val directory: File,
+    private var listener: OnTaskListener?
+): AsyncTask<Void, Int, Exception>(){
+
+    interface OnTaskListener {
+        fun onStartTask()
+        fun onProgressTask(progress: Int)
+        fun onCompleteTask(e: Exception?)
+    }
+
+    companion object {
+        private const val TAG = "ExcelConverterTask"
+
+        // file name
+        private const val EXCEL_FILE_NAME = "output.xlsx"
+
+        // sheet name
+        private const val DEFAULT_SHEET_NAME = "관리품목"
+
+        private val columnArray = arrayOf(
+            ColumnInfo(CELL_INDEX_NO, CellType.STRING, "No"),
+            ColumnInfo(CELL_INDEX_LABEL, CellType.STRING, "라벨번호"),
+            ColumnInfo(CELL_INDEX_IMAGE, CellType.STRING, "사진"),
+            ColumnInfo(CELL_INDEX_LOCATION, CellType.STRING, "위치"),
+            ColumnInfo(CELL_INDEX_NAME, CellType.STRING, "품명"),
+            ColumnInfo(CELL_INDEX_MANUFACTURER, CellType.STRING, "제조사"),
+            ColumnInfo(CELL_INDEX_MODEL, CellType.STRING, "모델명"),
+            ColumnInfo(CELL_INDEX_MANUFACTURE_DATE, CellType.STRING, "제조일자"),
+            ColumnInfo(CELL_INDEX_CONDITION, CellType.STRING, "상태"),
+            ColumnInfo(CELL_INDEX_SIZE, CellType.STRING, "규격"),
+            ColumnInfo(CELL_INDEX_AMOUNT, CellType.STRING, "수량")
+        )
+
+        @JvmStatic
+        fun convert(productList: List<Product>, directory: File, listener: OnTaskListener?): ExcelConverterTask {
+            return ExcelConverterTask(productList, directory, listener).apply {
+                executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+            }
+        }
+    }
+
+    override fun onPreExecute() {
+        DKLog.info(TAG) { "onPreExecute()" }
+        listener?.run {
+            onStartTask()
+        }
+    }
+
+    override fun doInBackground(vararg params: Void?): Exception? {
+        DKLog.info(TAG) { "doInBackground()" }
+
+        try {
+            if (checkDirectory()) {
+                val targetFile = File(directory, EXCEL_FILE_NAME).apply {
+                    if (exists()) throw Exception("Exist target file : $absolutePath")
+                }
+
+                saveExcelFile(targetFile)
+            }
+        } catch (e: Exception) {
+            DKLog.error(TAG) { "doInBackground() - error : ${e.message}" }
+            return e
+        }
+
+        return null
+    }
+
+    override fun onProgressUpdate(vararg progress: Int?) {
+        DKLog.info(TAG) { "onProgressUpdate() - progress : ${progress[0]}" }
+
+        listener?.run {
+            onProgressTask(progress[0] ?: 0)
+        }
+    }
+
+    override fun onPostExecute(e:Exception?) {
+        DKLog.info(TAG) { "onPostExecute()" }
+
+        listener?.run {
+            onCompleteTask(e)
+        }
+    }
+
+    override fun onCancelled(e:Exception?) {
+        DKLog.debug(TAG) { "onCancelled() - exception : ${e?.message ?: ""} " }
+
+        super.onCancelled(e)
+    }
+
+    override fun onCancelled() {
+        DKLog.debug(TAG) { "onCancelled(" }
+
+        super.onCancelled()
+    }
+
+    @Throws(Exception::class)
+    private fun checkDirectory(): Boolean {
+        return directory.run {
+            if (!exists() && !mkdirs()) {
+                throw Exception("Failed to create directory.")
+            }
+
+            true
+        }
+    }
+
+    @Throws(Exception::class)
+    private fun saveExcelFile(file: File) {
+        DKLog.info(TAG) { "saveExcelFile() - file : ${file.absolutePath}" }
+
+//        val workBook = XSSFWorkbook()
+        val workBook = HSSFWorkbook()
+        workBook.createSheet(getNextSheetName(workBook)).let { sheet ->
+            sheet.createRow(0).apply {
+                columnArray.forEach { column ->
+                    createCell(column.index, column.type).apply {
+                        setCellValue(column.name)
+                    }
+                }
+            }
+
+            for (itemIndex in 0..productList.lastIndex) {
+                writeProductData(productList[itemIndex], sheet.createRow(itemIndex + 1))
+                publishProgress((itemIndex + 1) * 100 / productList.size)
+            }
+        }
+
+        file.outputStream().use {
+            workBook.write(it)
+        }
+    }
+
+    private fun getNextSheetName(workBook: HSSFWorkbook) = "$DEFAULT_SHEET_NAME-${workBook.numberOfSheets + 1}"
+
+    private fun writeProductData(product: Product, row: HSSFRow) {
+        DKLog.info(TAG) { "writeProductData() - product : $product" }
+
+        columnArray.forEach { column ->
+            row.createCell(column.index, column.type).apply {
+                when (column.index) {
+                    CELL_INDEX_NO -> {
+                        setCellValue(product.id.toString())
+                    }
+
+                    CELL_INDEX_LABEL -> {
+                        setCellValue(product.label)
+                    }
+
+                    CELL_INDEX_IMAGE -> {
+                        setCellValue(product.imagePath)
+                    }
+
+                    CELL_INDEX_LOCATION -> {
+                        setCellValue(product.location)
+                    }
+
+                    CELL_INDEX_NAME -> {
+                        setCellValue(product.name)
+                    }
+
+                    CELL_INDEX_MANUFACTURER -> {
+                        setCellValue(product.manufacturer)
+                    }
+
+                    CELL_INDEX_MODEL -> {
+                        setCellValue(product.model)
+                    }
+
+                    CELL_INDEX_MANUFACTURE_DATE -> {
+                        setCellValue(product.manufactureDate)
+                    }
+
+                    CELL_INDEX_CONDITION -> {
+                        setCellValue(product.condition.name)
+                    }
+
+                    CELL_INDEX_SIZE -> {
+                        setCellValue(product.size)
+                    }
+
+                    CELL_INDEX_AMOUNT -> {
+                        setCellValue(product.amount.toString())
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class ColumnInfo(
+    val index: Int,
+    val type: CellType,
+    val name: String
+) {
+    companion object {
+
+        // cell index
+        internal const val CELL_INDEX_NO = 0
+        internal const val CELL_INDEX_LABEL = 1
+        internal const val CELL_INDEX_IMAGE = 2
+        internal const val CELL_INDEX_LOCATION = 3
+        internal const val CELL_INDEX_NAME = 4
+        internal const val CELL_INDEX_MANUFACTURER = 5
+        internal const val CELL_INDEX_MODEL = 6
+        internal const val CELL_INDEX_MANUFACTURE_DATE = 7
+        internal const val CELL_INDEX_CONDITION = 8
+        internal const val CELL_INDEX_SIZE = 9
+        internal const val CELL_INDEX_AMOUNT = 10
+    }
+}
--- app/src/main/java/com/deky/productmanager/ui/MainFragment.kt
+++ app/src/main/java/com/deky/productmanager/ui/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.deky.productmanager.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +8,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.deky.productmanager.R
 import com.deky.productmanager.database.ProductDB
-import com.deky.productmanager.database.entity.Condition
-import com.deky.productmanager.database.entity.Product
 import com.deky.productmanager.databinding.MainFragmentBinding
+import com.deky.productmanager.excel.ExcelConverterTask
 import com.deky.productmanager.model.MainViewModel
+import com.deky.productmanager.util.FileUtils
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.io.File
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainFragment : BaseFragment() {
@@ -25,6 +26,8 @@ class MainFragment : BaseFragment() {
     }
 
     private lateinit var dataBinding: MainFragmentBinding
+
+    private var excelTask: ExcelConverterTask? = null
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProviders.of(this@MainFragment)[MainViewModel::class.java]
@@ -70,8 +73,34 @@ class MainFragment : BaseFragment() {
         }
 
         btn_test.setOnClickListener {
-            context?.let {
-                ProductDB.getInstance(it).setSampleData()
+            if (excelTask != null) return@setOnClickListener
+
+            context?.let { context ->
+                ProductDB.getInstance(context).run {
+                    setSampleData()
+
+                    SimpleDateFormat("yyyy년 MM월dd일 HH시mm분ss초", Locale.getDefault()).format(System.currentTimeMillis()).let {
+                        val directory = File(FileUtils.getDataDirectory(context), it)
+                        excelTask = ExcelConverterTask.convert(productDao().getAll(), directory, object: ExcelConverterTask.OnTaskListener{
+                            override fun onStartTask() {
+                                log.debug { "ExcelConverterTask.onStartTask()" }
+                            }
+
+                            override fun onProgressTask(progress: Int) {
+                                log.debug { "ExcelConverterTask.onProgressTask() - progress : $progress" }
+                            }
+
+                            override fun onCompleteTask(e: Exception?) {
+                                log.debug { "ExcelConverterTask.onCompleteTask()" }
+                                e?.let {
+                                    log.error(true) { "Error : ${e.message}" }
+                                }
+
+                                excelTask = null
+                            }
+                        })
+                    }
+                }
             }
         }
     }
