85fc842455a587fa7b05fa98dd81cc256ebed351
diane.10 <diane.10@kakaocorp.com>
2020-11-15 20:47:39 +0900

    [#11] 앱 사용시 화면 off 안되도록

--- app/src/main/java/com/deky/productmanager/MainActivity.kt
+++ app/src/main/java/com/deky/productmanager/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.deky.productmanager
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.deky.productmanager.ui.MainFragment
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
